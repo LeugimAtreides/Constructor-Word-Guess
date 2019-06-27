290ed0323a054b35d28fca44c32e0c2d0897cef6
LeugimAtreides <m.villarreal789@hotmail.com>
2019-06-27 18:48:35 -0400

    Constructor Word Guess 1.0.0: Added js files that will hold game logic, and added pseudocode to being the planning process

---
+++ index.js
@@ -0,0 +1,18 @@
+// Set valuable to require inquirer
+var inquire = require("inquirer");
+
+// index.js: The file containing the logic for the course of the game,
+
+// which depends on Word.js
+var word = require("./word.js");
+
+// and:
+
+// Randomly selects a word and uses the Word constructor to store it
+function selectWord(){};
+
+// Prompts the user for each guess 
+function playGame(){};
+
+// and keeps track of the user's remaining guesses
+function keepTrack(){};
