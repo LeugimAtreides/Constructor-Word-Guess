d4bcb7485b5c78f51642d2c0cdcea1cea2a86d7d
LeugimAtreides <m.villarreal789@hotmail.com>
2019-07-09 12:15:28 -0400

    Constructor-Word-Guess: Fixed several issues and achieved functionality

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-// Set valuable to require inquirer
-var inquire = require("inquirer");
+var Game = require("./lib/Game");
 
-// index.js: The file containing the logic for the course of the game,
+// Initialize a new Game object
+var game = new Game();
 
-// which depends on Word.js
-var word = require("./word.js");
-
-// and:
-
-// Randomly selects a word and uses the Word constructor to store it
-function selectWord(){};
-
-// Prompts the user for each guess 
-function playGame(){};
-
-// and keeps track of the user's remaining guesses
-function keepTrack(){};
+// Start playing
+game.play();
